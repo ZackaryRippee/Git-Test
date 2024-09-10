c607c83689c911e1a986425b6ff20907b33e8459
Zack Rippee <zrippee@cs1-test.utm.edu>
2024-09-09 21:18:43 -0500

    new main that asks for number and sums all numbers 1 to n

--- git_test/main.cpp
+++ git_test/main.cpp
@@ -2,9 +2,22 @@
 #include <iostream>
 using namespace std;
 
+int sum (int number);
+
 int main (){
-
-  cout << "Hello World!" << endl;
-
+  int userNumber = 0;
+  cout << "Enter a number: ";
+  cin >> userNumber;
+  cout << endl << "Sum: " << sum(userNumber);
+  
   return 0;
+}
+
+int sum(int number){
+  int value = 0;
+  for (int i = 0; i < number; i++){
+    value = i + value;
+  }
+  value = value + number;
+  return value;
 }
